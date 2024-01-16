37c6e0ef2cd136fa0a8201f0ad08dd2964882ae0
Hoshimikan6490 <88366094+Hoshimikan6490@users.noreply.github.com>
2024-01-16 13:55:01 +0900

    default.jsとexample.jsを一本化

--- example.js
+++ example.js
@@ -6,7 +6,15 @@ http
   })
   .listen(8080);
 
-const { Client, GatewayIntentBits } = require("discord.js");
+// Discord bot implements
+const {
+  Client,
+  GatewayIntentBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+} = require("discord.js");
+require("dotenv").config();
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -14,24 +22,91 @@ const client = new Client({
     GatewayIntentBits.GuildMessages,
   ],
 });
-require("dotenv").config();
-
-//起動確認
-client.once("ready", () => {
-  console.log(`${client.user.tag} Ready`);
-});
-
-//返答
-client.on("messageCreate", async (message) => {
-  if (message.author.bot) {
-    return;
-  }
-
-  if (message.content == "hi") {
-    message.channel.send("hi!");
-  }
-});
-
-//Discordへの接続
+const prefix = "pj!";
 const token = process.env.token;
+
+// botが準備できれば発動され、 上から順に処理される。
+client.on("ready", () => {
+  // コンソールにReady!!と表示
+  console.log("Ready!!");
+
+  // ステータスを設定する
+  setInterval(() => {
+    client.user.setActivity({
+      name: `所属サーバー数は、${client.guilds.cache.size}サーバー｜　Ping値は、${client.ws.ping}msです`,
+    });
+  }, 10000);
+  client.channels.cache.get("889486664760721418").send("起動しました！");
+
+  // readyイベントここまで
+});
+
+// botがメッセージを受信すると発動され、 上から順に処理される。
+client.on("messageCreate", async (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(prefix.length).trim().split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command == "about") {
+    message.channel.send({
+      embeds: [
+        {
+          title: "Planet bot β (JS)について",
+          description: "node.jsで作成された、BOT開発テスト用のbotです。",
+          color: 3823616,
+          timestamp: new Date(),
+          thumbnail: {
+            url: "attachment://logo.png",
+          },
+          footer: {
+            text: "This bot is made by Hoshimikan6490",
+            icon_url: "attachment://me.png",
+          },
+        },
+      ],
+      files: [
+        {
+          attachment: "images/logo.png",
+          name: "logo.png",
+        },
+        {
+          attachment: "images/me.png",
+          name: "me.png",
+        },
+      ],
+    });
+  } else if (command == "support") {
+    const help_start = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("support")
+        .setLabel("お問い合わせを始める")
+        .setStyle(ButtonStyle.Primary)
+        .setEmoji("🎫")
+    );
+    //button作る
+    await message.channel.send({
+      embeds: [
+        {
+          title: "🎫お問い合わせ🎫",
+          description:
+            "質問・要望・バグ報告がある場合は下のボタンを押してください。\nサポートチームが対応いたします。",
+          color: 0x00eaff,
+          footer: {
+            text: "↓ここをクリックして始めてください↓",
+          },
+        },
+      ],
+      components: [help_start],
+    });
+  }
+});
+
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.customId === "support") {
+    await interaction.reply("hi!!!");
+  }
+});
+
+// botログイン
 client.login(token);
